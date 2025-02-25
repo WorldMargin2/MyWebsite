5a39a7963da025185f2e1a77e2f790673d38cd38
WorldMargin2 <148187588+WorldMargin2@users.noreply.github.com>
2025-02-25 09:52:00 +0800

    Update index.js

--- webfile/JS/index.js
+++ webfile/JS/index.js
@@ -1,9 +1,12 @@
-$(document).ready(function() {
-
 let tmp_w = 0;
 let tmp_h = 0;
 let tmp_w_center = 0;
 let tmp_h_center = 0;
+let scale;
+
+$(document).ready(function() {
+
+
 $(document).on("scroll", function() {
     const title = $("#title");
     const vh=window.innerHeight;
@@ -24,8 +27,8 @@ $(document).on("scroll", function() {
     scale=(1-(s_t/vh));
 
     changed_left = tmp_w_center * scale;
-    if(75*scale<=tmp_w){
-        changed_left > 0 ? changed_left : 0;
+    if(tmp_w*scale<=vw){
+        changed_left=changed_left > 0 ? changed_left : 0;
     }
     changed_top = tmp_h_center*scale;
     changed_top = changed_top > 0 ? changed_top : 0;
